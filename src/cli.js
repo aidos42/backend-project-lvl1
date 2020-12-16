25e781f8489673c1f51c12dd0fe33dcf923450af
Aidos <aidos49@yahoo.com>
2020-12-16 15:11:56 +0300

    fix some minor mistakes in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
-export default () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export default greeting;
