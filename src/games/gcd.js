d10ed4083001da12a9000586c476376e75c73470
Aidos <aidos49@yahoo.com>
2020-12-22 18:23:31 +0300

    add some minor fix in gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 import getRandomNumber from '../utilities.js';
+import isAnswerCorrect from '../isAnswerCorrect.js';
 
 const getGCD = (numOne, numTwo) => {
   let a = numOne;
@@ -22,10 +23,7 @@ const playGame = (name) => {
     console.log(`Question: ${numOne} ${numTwo}`);
     const answer = Number(readlineSync.question('Your answer: '));
     const expectedAnswer = getGCD(numOne, numTwo);
-    if (answer === expectedAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${name}!`);
+    if (!isAnswerCorrect(expectedAnswer, answer, name)) {
       return;
     }
   }
